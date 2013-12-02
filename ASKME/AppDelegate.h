f918ba7fe57ce935703b482f8a31c8a3ebdfffa4
Luis Miguel <luismi@lmcomunicacion.es>
2013-12-02 22:25:34 +0100

    Initial Commit

---
+++ ASKME/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ASKME
+//
+//  Created by LUISMI on 02/12/13.
+//  Copyright (c) 2013 LUISMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ASKME/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ASKME
+//
+//  Created by LUISMI on 02/12/13.
+//  Copyright (c) 2013 LUISMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
